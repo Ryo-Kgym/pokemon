f98fa72119f9f844d76ab82def2672d6750ce735
Ryo-Kgym <mraytoh@icloud.com>
2023-10-17 13:33:29 +0900

    属性クラスを作成

---
+++ pokemon-core/src/main/java/pokemon/core/domain/model/attribute/Attribute.java
@@ -0,0 +1,23 @@
+package pokemon.core.domain.model.attribute;
+
+public enum Attribute {
+    NORMAL, // ノーマル
+    FIRE, // ほのお
+    WATER, // みず
+    GRASS, // くさ
+    ELECTRIC, // でんき
+    ICE, // こおり
+    FIGHTING, // かくとう
+    POISON, // どく
+    GROUND, // じめん
+    FLYING, // ひこう
+    PSYCHIC, // エスパー
+    BUG, // むし
+    ROCK, // いわ
+    GHOST, // ゴースト
+    DRAGON, // ドラゴン
+    DARK, // あく
+    STEEL, // はがね
+    FAIRY, // フェアリー
+    ;
+}
