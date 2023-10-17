fedb8ead43814dd7ff6084dd6021b6c23efdfb5e
Ryo-Kgym <mraytoh@icloud.com>
2023-10-17 13:24:54 +0900

    core プロジェクトを作成した。

---
+++ pokemon-core/src/test/java/pokemon/core/PokemonCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pokemon.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
