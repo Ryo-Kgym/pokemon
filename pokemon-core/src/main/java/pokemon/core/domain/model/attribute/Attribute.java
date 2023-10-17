352af141e6e00c18f87efcf22e6b8f515eb06678
Ryo-Kgym <mraytoh@icloud.com>
2023-10-17 15:44:27 +0900

    相手の属性の効果性を返すメソッドを作成した。

--- pokemon-core/src/main/java/pokemon/core/domain/model/attribute/Attribute.java
+++ pokemon-core/src/main/java/pokemon/core/domain/model/attribute/Attribute.java
@@ -1,5 +1,7 @@
 package pokemon.core.domain.model.attribute;
 
+import java.util.Arrays;
+
 public enum Attribute {
     NORMAL, // ノーマル
     FIRE, // ほのお
@@ -20,4 +22,14 @@ public enum Attribute {
     STEEL, // はがね
     FAIRY, // フェアリー
     ;
+
+    public Effectiveness getEffectiveness(Attribute opponent) {
+
+        return Arrays.stream(Effectiveness.values())
+                .filter(e -> e.getJudger()
+                        .map(j -> j.getTarget(this).contains(opponent))
+                        .orElse(false))
+                .findFirst()
+                .orElse(Effectiveness.NORMAL);
+    }
 }
